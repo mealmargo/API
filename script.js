755241f275188f74e145d33714616a940a41b309
mealmargo <129554597+mealmargo@users.noreply.github.com>
2023-06-11 03:08:11 +0300

    added condition if (response.ok)

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ formEl.addEventListener('submit', async (e) => {
   e.preventDefault(); //чтобы при клике на поиск страница не обновилась
   const inputsValue = Object.fromEntries(new FormData(e.target)); // получить значения инпутов , object.fromEntries вернет массив в виде объекта , e target вся наша форма
   const response = await fetch(` https://api.github.com/users/${inputsValue.name} `); //создаем запрос на адрес
+  
+if (response.ok) {
+    const data = await response.json(); //здесь будет лежать информация , которая придет от API
+    wrapper.appendChild(createProfileEl(data)) 
+    mainEl.appendChild(wrapper); //присваеваем main значения враппера
+    inputEl.value = '';
+  } else {
+    alert("Пользователь не найден")
+  }
+})
